3c98ca4612fc16091120e5c833e837c77d747f8e
luisavilaland <luisavilaland@gmail.com>
2026-02-16 20:53:29 -0300

    fix: add missing gtag helper and google analytics component

---
+++ components/google-analytics.tsx
@@ -0,0 +1,24 @@
+import Script from "next/script";
+
+const GA_MEASUREMENT_ID = "G-HYDZGD57JB";
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            anonymize_ip: true
+          });
+        `}
+      </Script>
+    </>
+  );
+}
